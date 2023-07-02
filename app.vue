80d4f17965209b35dd0b467b6f7b47d5d4c1eab6
Luciano Weslen <luciano.weslen@zrp.com.br>
2023-07-02 11:51:02 -0300

    feat: added tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="h-screen flex justify-center items-center flex-col gap-2">
+    <img src="/logo.svg" alt="Logo" class="h-12"/>
+    <span class="text-3xl font-bold">@luweslen</span>
   </div>
 </template>
+
+<script lang="ts">
+useHead({
+  link: [
+    {rel: 'icon', type: 'image/svg+xml', href: './favicon.svg'}
+  ]
+})
+</script>
